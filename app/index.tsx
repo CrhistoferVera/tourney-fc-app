38fca03275e3f964affcd6f1997193f570f5117f
CrhistoferVera <cristofervera110@gmail.com>
2026-04-14 10:33:45 -0400

    initial config

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-gray-800">Tourney FC</Text>
+      <Text className="mt-2 text-base text-gray-500">
+        BIENVENIDOS A TOURNEYFC BROU
+      </Text>
+    </View>
+  );
+}
